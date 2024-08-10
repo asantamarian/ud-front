d4bb65475ce1b0e656fe52ebf8227a2d538ed292
andres <asantnor@gmail.com>
2024-08-09 21:10:26 -0400

    imagenes y home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image'
 export default function Home () {
   return (
-    <div>
-      <h1>Unión Dragones</h1>
+    <div className="flex items-center justify-center h-screen ">
+        <div className="text-center">
+            <div className="w-lvw h-32 mx-auto mb-8">
+                <Image
+                    src="/brand/icono_texto.png"
+                    alt="Coming Soon"
+                    layout="fill"
+                    objectFit="contain"
+                    priority
+                />
+            </div>
+        </div>
     </div>
   )
 }
